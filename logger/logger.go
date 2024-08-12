cbc55094e7d71691da09cb5147596ba7ed0901d2
felix314159 <felix314159@users.noreply.github.com>
2024-08-12 15:10:12 +0200

    improved logging (old log is now deleted at startup if it exists)

--- logger/logger.go
+++ logger/logger.go
@@ -12,14 +12,26 @@ import (
     "os"
 )
 
+const logFilePath = "app.log" // its in rootdir, or when running tests in every cwd
+
 var (
     // L is the instance of the logger used across the program
     L *log.Logger
 )
 
-func init() { // init is automatically run if this package is imported, 
+func init() { // init is automatically run if this package is imported
+    // delete old log file if it exists
+    err := os.Remove(logFilePath)  // ignore err that is returned (either file is deleted or file doesnt exist. either way file won't exist after this)
+    if err != nil {
+        // only panic if the error is something other than "file does not exist"
+        if !os.IsNotExist(err) {
+            panic(err)
+        } 
+
+    }
+
     // open log file
-    logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666) // append to log if it exists, otherwise create it
+    logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666) // append to log if it exists, otherwise create it
     if err != nil {
         log.Fatalln("Failed to open log file:", err)
     }
